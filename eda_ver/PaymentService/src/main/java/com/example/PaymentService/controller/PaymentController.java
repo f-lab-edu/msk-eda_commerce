fee8dd595afe466684316035f504ef68fd4efd04
wayman1q@W <wayman7@bitbucket.org>
2024-10-03 20:24:25 +0900

    first commit

---
+++ eda_ver/PaymentService/src/main/java/com/example/PaymentService/controller/PaymentController.java
@@ -0,0 +1,32 @@
+package com.example.PaymentService.controller;
+
+import com.example.PaymentService.dto.PaymentMethodDto;
+import com.example.PaymentService.entity.Payment;
+import com.example.PaymentService.entity.PaymentMethod;
+import com.example.PaymentService.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class PaymentController {
+    @Autowired
+    PaymentService paymentService;
+
+    @PostMapping("/payment/methods")
+    public PaymentMethod registerPaymentMethod(@RequestBody PaymentMethodDto dto) {
+        return paymentService.registerPaymentMethod(
+              dto.userId,
+              dto.paymentMethodType,
+              dto.creditCardNumber
+        );
+    }
+
+    @GetMapping("/payment/users/{userId}/first-method")
+    public PaymentMethod getPaymentMethod(@PathVariable Long userId) {
+        return paymentService.getPaymentMethodByUser(userId);
+    }
+    @GetMapping("/payment/payments/{paymentId}")
+    public Payment getPayment(@PathVariable Long paymentId) {
+        return paymentService.getPayment(paymentId);
+    }
+}
